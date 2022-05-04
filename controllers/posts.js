bb8e9ab6e06d343928f6eb1ec69defa7c27fbae3
ella <yanzi19841227@gmail.com>
2022-05-04 19:12:08 +0800

    修改單筆刪除 url 未帶入 id 變成刪除全部貼文 bug

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,71 +1,79 @@
-const Post = require('../models/posts');
-const successHandle = require('../service/successHandle');
-const { errorHandle } = require('../service/errorHandle');
-const checkBodyRequired = require('../tools/checkBodyRequired');
+const Post = require("../models/posts");
+const successHandle = require("../service/successHandle");
+const { errorHandle } = require("../service/errorHandle");
+const checkBodyRequired = require("../tools/checkBodyRequired");
 
 const posts = {
-    async getPosts(req, res, next) {
-        // 貼文時間：新到舊
-        const posts = await Post.find().sort({createdAt: -1});
-        successHandle(res, posts);
-    },
-    async addPost(req, res, next) {
-        try{
-          const data = req.body;
-          const bodyResultIsPass = checkBodyRequired(req.method, res, data);
-      
-          if(bodyResultIsPass){
-            const newPost = await Post.create({
-              'name': data.name,
-              'image': data.image,
-              'content': data.content,
-              'type': data.type,
-              'tags': data.tags
-            });
-      
-            successHandle(res, newPost);
-          }
-        }catch(error){
-          console.error(error);
-          errorHandle(res, 400, error.message);
-        }
-    },
-    async delPosts(req, res, next) {
-        await Post.deleteMany({});
-        successHandle(res, []);
-    },
-    async delPost(req, res, next) {
-        const id = req.params.id;
-        await Post.findByIdAndDelete(id)
-          .then(delPost => {
-            if(delPost._id){ successHandle(res, delPost) }
-          })
-          .catch(error => {
-            console.error(error);
-            errorHandle(res, 400, 'id');
-          });
-    },
-    async etidPost(req, res, next) {
-        try{
-          const id = req.params.id;
-          const data = req.body;
-          const bodyResultIsPass = checkBodyRequired(req.method, res, data);
-      
-          if(bodyResultIsPass){
-            await Post.findByIdAndUpdate(id, data, {new: true, runValidators:true})
-              .then(update => {
-                if(update){ 
-                  successHandle(res, update)
-                }else{
-                  errorHandle(res, 400, 'id');
-                }
-              });  
-          }
-        }catch(error){
-          const err = error.path === '_id' ? 'id' : error.message;
-          errorHandle(res, 400, err);
-        }
+  async getPosts(req, res, next) {
+    // 貼文時間：新到舊
+    const posts = await Post.find().sort({ createdAt: -1 });
+    successHandle(res, posts);
+  },
+  async addPost(req, res, next) {
+    try {
+      const data = req.body;
+      const bodyResultIsPass = checkBodyRequired(req.method, res, data);
+
+      if (bodyResultIsPass) {
+        const newPost = await Post.create({
+          name: data.name,
+          image: data.image,
+          content: data.content,
+          type: data.type,
+          tags: data.tags,
+        });
+
+        successHandle(res, newPost);
+      }
+    } catch (error) {
+      console.error(error);
+      errorHandle(res, 400, error.message);
     }
+  },
+  async delPosts(req, res, next) {
+    if (req.url === "/posts") {
+      await Post.deleteMany({});
+      successHandle(res, []);
+    } else {
+      errorHandle(res, 400, "routing");
+    }
+  },
+  async delPost(req, res, next) {
+    const id = req.params.id;
+    await Post.findByIdAndDelete(id)
+      .then((delPost) => {
+        if (delPost._id) {
+          successHandle(res, delPost);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        errorHandle(res, 400, "id");
+      });
+  },
+  async etidPost(req, res, next) {
+    try {
+      const id = req.params.id;
+      const data = req.body;
+      const bodyResultIsPass = checkBodyRequired(req.method, res, data);
+
+      if (bodyResultIsPass) {
+        await Post.findByIdAndUpdate(id, data, {
+          new: true,
+          runValidators: true,
+        }).then((update) => {
+          if (update) {
+            successHandle(res, update);
+          } else {
+            errorHandle(res, 400, "id");
+          }
+        });
+      }
+    } catch (error) {
+      const err = error.path === "_id" ? "id" : error.message;
+      errorHandle(res, 400, err);
+    }
+  },
 };
 
 module.exports = posts;
